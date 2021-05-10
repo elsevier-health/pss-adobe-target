6d65d725bb5935df7d8c07ad69a8c0ce752ceec4
Stephen Stephenson <s.stephenson@elsevier.com>
2021-05-10 11:43:54 -0600

    prepping initial PR

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-
-// ============================================================
-// Requires
-// ============================================================
-
-const targetRouterConstructor = require("./lib/TargetRouter");
-const targetServiceConstructor = require("./lib/TargetService");
-
-// ============================================================
-// Exports
-// ============================================================
-
 exports = module.exports = {
-    Router: targetRouterConstructor,
-    Service: targetServiceConstructor
+
 };
