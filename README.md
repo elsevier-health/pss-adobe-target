35308849fd885c310796510d3bade0593e7c368f
Stephen Stephenson <37632017+StephenGStephenson@users.noreply.github.com>
2021-05-17 11:05:12 -0600

    Feature/amp 354 initial implementation (#1)
    * AMP-354 updated documentation
    
    * adding unit tests of business logic
    
    * AMP-354 updated documentation
    
    * adding unit tests of business logic
    
    * adding a gitignore file
    
    * removing console.log
    
    * AMP-354 added missed unit test
    
    * AMP-354 adding a sequence diagram to README.md
    
    * AMP-354 fixing typos in diagram
    
    * Adding backticks for values in tables
    
    * Wrapping links in <>
    
    * Fixing typo in README
    
    * Improving the descriptions of the routes setup by the Router.
    
    * Improved documentation of route query parameters
    
    * AMP-354 updated documentation
    
    * adding unit tests of business logic
    
    * adding a gitignore file
    
    * removing console.log
    
    * AMP-354 added missed unit test
    
    * AMP-354 adding a sequence diagram to README.md
    
    * AMP-354 fixing typos in diagram
    
    * Adding backticks for values in tables
    
    * Wrapping links in <>
    
    * Fixing typo in README
    
    * Improving the descriptions of the routes setup by the Router.
    
    * Improved documentation of route query parameters
    
    * Fixing unintentional issued created when I prepped the PR
    
    * adding error to responses of the routes
    
    * adding error returns for middleware functions
    
    * adding error to the return value of middleware errors
    
    * Clarifying the exports of TargetService

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # pss-adobe-target
 
-This module is a wrapper around the pss implementation of Adobe Target.
+This module is a wrapper around the Adobe Target Node SDK. The module exports two constructors for use by products. One creates an express router with routes defined to get Adobe Target activities and to send notifications to Adobe Target. The other contructor creates a service that can be used to get activities or send notifications in a couple of ways, with middleware or calling  functions directly.
+
+![Alt text](./assets/img/pss-adobe-target.svg)
 
 ## Installation
 
-In the server directory of a product run: 
+In the server directory of a product run:
 
 ```npm i -P @pss/pss-adobe-target```
 
@@ -14,7 +16,7 @@ Both the pss-adobe-target Router and Service expect the follow configuration at 
 
 ```javascript
 const pssAdobeTargetRouter = PssAdobeTarget.Router(adobeTargetConfig, PssLogger);
-const pssAdobeTargetRouter = PssAdobeTarget.Service(adobeTargetConfig, PssLogger);
+const pssAdobeTargetService = PssAdobeTarget.Service(adobeTargetConfig, PssLogger);
 ```
 
 ### pss-adobe-target configuration
@@ -34,45 +36,53 @@ const pssAdobeTargetRouter = PssAdobeTarget.Service(adobeTargetConfig, PssLogger
 }
 ```
 
-For more information about the Adobe Target SDK configuration see this website: https://adobetarget-sdks.gitbook.io/docs/sdk-reference-guides/nodejs-sdk/initialize-sdk.
+
+For more information about the Adobe Target SDK configuration see this website: <https://adobetarget-sdks.gitbook.io/docs/sdk-reference-guides/nodejs-sdk/initialize-sdk>.
 
 | attribute | required | type | expected value | secret | description |
 | --------- | -------- | ---- | --------------- | ------ | ----------- |
-| client | yes | string | vault_adobe_target_client | yes | The Adobe Target client code. This value will be replaced with a secret from vault. |
-| organizationId | yes | string | vault_adobe_target_org_id | yes | The Adobe Tartet IMS Id. The value will be replaced with a secret from vault | 
-| propertyToken | no | string | vault_adobe_target_property | yes | The Adobe Target environment property. The value will be replaced with a secret from vault. | 
-| decisioningMethod | no | string | on-device, server-side, or hybrid | no | Determines the decisioning method to use | 
-| events | no | object | see below | no | An optional object with event name keys and callback function values |
-| timeout | no | number | Timeout in milleseconds | no | The timeout value for all requests to Adobe Target. | 
+| `client` | yes | string | `vault_adobe_target_client` | yes | The Adobe Target client code. This value will be replaced with a secret from vault. |
+| `organizationId` | yes | string | `vault_adobe_target_org_id` | yes | The Adobe Tartet IMS Id. The value will be replaced with a secret from vault | 
+| `propertyToken` | yes | string | `vault_adobe_target_property` | yes | The Adobe Target environment property. The value will be replaced with a secret from vault. | 
+| `decisioningMethod` | no | string | `on-device`, `server-side`, or `hybrid` | no | Determines the decisioning method to use | 
+| `events` | no | object | see below | no | An optional object with event name keys and callback function values |
+| `timeout` | no | number | Timeout in milleseconds | no | The timeout value for all requests to Adobe Target. | 
 
 ### Events
 
-These event functions will be defined either in code before initializing pss-adobe-target or in the configuration file. For more information about the Adobe Target SDK events see this website: https://adobetarget-sdks.gitbook.io/docs/sdk-reference-guides/nodejs-sdk/sdk-events.
+These event functions will be defined either in code before initializing pss-adobe-target or in the configuration file. For more information about the Adobe Target SDK events see this website: <https://adobetarget-sdks.gitbook.io/docs/sdk-reference-guides/nodejs-sdk/sdk-events>.
 
-| attribute | required | type | description | 
-| clientReady | no | function | Emitted when the artifact has downloaded and the SDK is ready for getOffers calls. Recommended when using on-device decisioning method. |
-| artifactDownloadSucceeded | no | function | Emitted each time a new artifact is downloaded. |
-| artifactDownloadFailed | no | function | Emitted each time an artifact fails to download. |
+| attribute | required | type | description |
+| --------- | -------- | ---- | ----------- |
+| `clientReady` | no | function | Emitted when the artifact has downloaded and the SDK is ready for getOffers calls. Recommended when using on-device decisioning method. |
+| `artifactDownloadSucceeded` | no | function | Emitted each time a new artifact is downloaded. |
+| `artifactDownloadFailed` | no | function | Emitted each time an artifact fails to download. |
+
 
 ### PssLogger
 
 This is an initialized PssLogger that will be passed into both the Router and Service.
 
+```javascript
+const PssLogger = require("@pss/pss-logger")(config.loggingConfig);
+```
+
 ## Exports
 
-pss-adobe-target exports to constructors on for a Router and one for a service.
+pss-adobe-target exports two constructors, one for an express Router and one for a Service.
 
-### Router
+### Routes
 
 The pss-adobe-target Router sets up two routes for use in client-side ajax requests.
 
-GET /target/activity?activity={activity mbox}&activity={activity mbox}
+GET /target/activity?activity=value1&activity=value2
 
-This route will fetch one or more experience the current user is in as well as the offer delivered by those experiences.
+This route fetches one or more experience the current user is in as well as the offer delivered by those experiences. The route expects one or more activity query parameters. The activity parameter is the main mbox of the adobe target activity defined when creating the activity in Adobe Target.
 
-PUT /target/notification?mbox={mbox name}&mbox={mbox name} 
+PUT /target/notification?mbox=mbox1&mbox=mbox2
 
-This route will send notifications to adobe target for one or more mboxes.
+This route will send notifications to adobe target for one or more mboxes. The route expects one or more mbox query parameters. The mboxes correspond to the metrics setup when creating the activity in Adobe Target. This is how we will collect success metrics for server-side activities.
+
 
 ## Usage
 
@@ -100,12 +110,15 @@ app.use(pssAdobeTargetRouter);
 #### GET /target/activity
 From client-side javascript send ajax requests to fetch which experience the current user is in and the offer delivered by the experience.
 
+The ```/target/activity``` route expects one or more ```activity=some-mbox-here``` query parameter.
+
+
 ```javascript
 const activityName = "statdx-hsearch";
 
 $.ajax("/target/activity?activity=" + activityName).done(function(response) {
     const statdxHsearchActivity = response[activityName];
-    
+
     // Do something with the feature flag returned by the activity.
 });
 ```
@@ -115,11 +128,11 @@ Here is a sample response for the ```/target/activity?activity=statdx-hsearch```
 ```json
 {
   "statdx-hsearch": {
-      "active": true,
-      "variation": "control",
-      "payload": {
-        "searchIndex": 0
-      }
+    "active": true,
+    "variation": "control",
+    "payload": {
+      "searchIndex": 0
+    }
   }
 }
 ```
@@ -127,6 +140,8 @@ Here is a sample response for the ```/target/activity?activity=statdx-hsearch```
 #### PUT /target/notification
 
 From client-side javascript send ajax requests to trigger events for currently running server-side activities.
+
+The ```/target/notification``` route expects one or more ```mbox=some-mbox-here``` query parameter.
 
 ```javascript
 const mbox = "mbox server-side-success";
@@ -143,11 +158,11 @@ Here is an example of the notification route response object
 
 ```json
 {
-    "status": 200,
-    "requestId": "7d62c85c07fc4fe3a4622a0f847903ce",
-    "id": {
-        "tntId": "67664e186ef54ffd9cb60b20ef66aceb.35_0"
-    }
+  "status": 200,
+  "requestId": "7d62c85c07fc4fe3a4622a0f847903ce",
+  "id": {
+    "tntId": "67664e186ef54ffd9cb60b20ef66aceb.35_0"
+  }
 }
 ```
 
@@ -165,12 +180,82 @@ const PssAdobeTarget = require("@pss/pss-adobe-target");
 const pssAdobeTargetService = PssAdobeTarget.Service(config.adobeTargetConfig, PssLogger);
 ```
 
-Then use the middleware on routes defined in the product.
+Then use the middleware on routes defined in the product. 
+
+The ```Middleware.getActivities``` expects one or more ```activity=activity-box``` query paramenter.
+
+The ```Middleware.sendNotification``` expects one or more ```mbox=mbox-name``` query paramenter.
 
 ```javascript
+    app.get("/test/target/activity",
+        targetService.Middleware.getActivities,
+        (req, res, next) => {
+            const activities = req.getTargetActivities();
+            const searchEndpoint = activities["statdx-hsearch"];
+            
+            let features = req.session.features;
+            features["statdx-hsearch"] = searchEndpoint;
+            
+            req.session.save(() => {
+                
+                if (features["statdx-hsearch"].variation === "contol") {
+                    // show the control experience
+                } else if (features["statdx-hsearch"].variation === "variation") {
+                    // show the variation experience
+                } else {
+                    // show the default experience
+                }
+                
+                ... // code continues
+            });
+        });
 
 
-
+    app.get("/test/target/notification",
+        targetService.Middleware.sendNotification,
+        (req, res, next) => {
+            const activities = req.getTargetActivities();
+            
+            const notificationResponse = req.getTargetNotificationResponse();
+            if (notificationResponse.status !== 200) {
+                // Do something if the notification failed to send
+            }
+        });
 ```
 
 ### In code
+
+The Service exports functions that can be used in nodejs code to interact with adobe target
+
+```javascript
+
+const targetService = PssAdobeTarget.Service(adobeTargetConfig, PssLogger);
+
+const renderSomePage = (req, res, next) => {
+    
+    ...
+    
+    targetService.getActivities(["show-some-feature"])
+        .then(response => {
+            logger.trace("Get activity response = ");
+            logger.trace(response);
+            
+            const showSomeFeature = response["show-some-feature"];
+            req.session.features["show-some-feature"] = showSomeFeature;
+            
+            req.session.save(() => {
+                res.render("/some/page", {
+                    showVariation: req.session.features["show-some-feature"].variation === "variation"
+                });    
+            });
+            
+        })
+        .catch(error => {
+            logger.error("Failed to get activities");
+            logger.error(error);
+            res.render("/error/page");
+        });
+};
+
+```
+
--- test/TargetService.test.js
+++ test/TargetService.test.js
@@ -3,11 +3,12 @@
 // Requires
 // ============================================================
 
+const { validate: uuidValidate } = require("uuid");
 const TargetService = require("../lib/TargetService");
 const PssLogger = require("@pss/pss-logger")({
     categories: {
         default: {
-            level: process.env.LOG_LEVEL || "trace",
+            level: process.env.LOG_LEVEL || "info",
             enableCallStack: true
         }
     }
@@ -41,19 +42,95 @@ const getTargetService = (targetConfig) => {
     return targetService
 };
 
+const validateNotificationRequest = (request, cookie) => {
+    expect(request).not.toBe(null);
+    expect(request).toBeInstanceOf(Object);
+    expect(request).toHaveProperty("request");
+    expect(request).toHaveProperty("targetCookie");
+    expect(request.targetCookie).toEqual(cookie);
+    expect(request.request).toBeInstanceOf(Object);
+    expect(request.request).toHaveProperty("notifications");
+    expect(request.request.notifications).toBeInstanceOf(Array);
+};
+
+const validateNotification = (notification, type, mbox) => {
+    expect(notification).not.toBe(null);
+
+    expect(notification).toBeInstanceOf(Object);
+    expect(notification).toHaveProperty("type");
+    expect(notification).toHaveProperty("timestamp");
+    expect(notification).toHaveProperty("id");
+    expect(notification).toHaveProperty("mbox");
+
+    expect(typeof notification.timestamp).toBe("number");
+    expect(typeof notification.type).toBe("string");
+    expect(typeof notification.id).toBe("string");
+    expect(notification.mbox).toBeInstanceOf(Object);
+    expect(notification.mbox).toHaveProperty("name");
+
+    expect(notification.mbox.name).toBe(mbox);
+    expect(notification.type).toBe(type);
+    expect(uuidValidate(notification.id)).toBe(true);
+};
+
 // ============================================================
 // Tests
 // ============================================================
 
-it("", () => {});
+describe("TargetService.createNotificationRequest", () => {
+    const targetService = TargetService(targetConfig, PssLogger);
 
+    it("returns a notification request if an array of mboxes, an eventType, and a targetCookie", () => {
+        const request = targetService.createNotificationRequest([], "", {});
 
-describe("TargetService.initializeTargetClient", () => {});
+        validateNotificationRequest(request, {});
+        expect(request.request.notifications).toHaveLength(0);
+    });
 
-describe("TargetService.createNotificationRequest", () => {});
+    it("returns a request with mbox events of type display if eventType !== click", () => {
+        const request = targetService.createNotificationRequest(["mbox"], "", {});
+
+        validateNotificationRequest(request, {});
+        expect(request.request.notifications).toHaveLength(1);
+
+        const notification = request.request.notifications[0];
+        validateNotification(notification, "display", "mbox");
+    });
+
+    it("returns a request with mbox events of type display if eventType === display", () => {
+        const request = targetService.createNotificationRequest(["mbox"], "display", {});
+
+        validateNotificationRequest(request, {});
+        expect(request.request.notifications).toHaveLength(1);
+
+        const notification = request.request.notifications[0];
+        validateNotification(notification, "display", "mbox");
+    });
+
+    it("returns a request with mbox events of type click if eventType === click", () => {
+        const request = targetService.createNotificationRequest(["mbox"], "click", {});
+
+        validateNotificationRequest(request, {});
+        expect(request.request.notifications).toHaveLength(1);
+
+        const notification = request.request.notifications[0];
+        validateNotification(notification, "click", "mbox");
+    });
+
+    it("returns a request containing the passed in target cookie", () => {
+        const cookie = {
+            funny: "business"
+        }
+        const request = targetService.createNotificationRequest(["mbox"], "click", cookie);
+        validateNotificationRequest(request, cookie);
+        expect(request.request.notifications).toHaveLength(1);
+
+        const notification = request.request.notifications[0];
+        validateNotification(notification, "click", "mbox");
+    });
+});
 
 describe("TargetService.validateConfig", () => {
-
 
     it("fails if targetConfig is undefined or null ", () => {
         const targetService = getTargetService(targetConfig);
@@ -83,24 +160,219 @@ describe("TargetService.validateConfig", () => {
         }
     });
 
-    it("fails if targetConfig.client is undefined, null, or not a string", () => {
+    it("fails if targetConfig.client is falsy", () => {
         const targetService = getTargetService(targetConfig);
 
+        try {
+            targetService.validateConfig({});
+            new TypeError("targetService.validateConfig should fail");
+        }
+        catch (error) {
+            expect(error.message).toBe("targetConfig.client must be provided");
+        }
 
+        try {
+            targetService.validateConfig({ client: undefined });
+            new TypeError("targetService.validateConfig should fail");
+        }
+        catch (error) {
+            expect(error.message).toBe("targetConfig.client must be provided");
+        }
 
+        try {
+            targetService.validateConfig({ client: null });
+            new TypeError("targetService.validateConfig should fail");
+        }
+        catch (error) {
+            expect(error.message).toBe("targetConfig.client must be provided");
+        }
+
+        try {
+            targetService.validateConfig({ client: false });
+            new TypeError("targetService.validateConfig should fail");
+        }
+        catch (error) {
+            expect(error.message).toBe("targetConfig.client must be provided");
+        }
+    });
+
+    it("fails if targetConfig.client is not a string", () => {
+        const targetService = getTargetService(targetConfig);
+
+        try {
+            targetService.validateConfig({ client: 8 });
+            new TypeError("targetService.validateConfig should fail");
+        }
+        catch (error) {
+            expect(error.message).toBe("targetConfig.client must be a string");
+        }
+
+        try {
+            targetService.validateConfig({ client: {} });
+            new TypeError("targetService.validateConfig should fail");
+        }
+        catch (error) {
+            expect(error.message).toBe("targetConfig.client must be a string");
+        }
+
+        try {
+            targetService.validateConfig({ client: true });
+            new TypeError("targetService.validateConfig should fail");
+        }
+        catch (error) {
+            expect(error.message).toBe("targetConfig.client must be a string");
+        }
+    });
+
+    it("fails if targetConfig.organizationId is falsy", () => {
+        const targetService = getTargetService(targetConfig);
+        const config = {
+            client: "client"
+        }
+
+        try {
+            targetService.validateConfig(config);
+            new TypeError("targetService.validateConfig should fail");
+        }
+        catch (error) {
+            expect(error.message).toBe("targetConfig.organizationId must be provided");
+        }
+
+        try {
+            config.organizationId = undefined;
+            targetService.validateConfig(config);
+            new TypeError("targetService.validateConfig should fail");
+        }
+        catch (error) {
+            expect(error.message).toBe("targetConfig.organizationId must be provided");
+        }
+
+        try {
+            config.organizationId = null;
+            targetService.validateConfig(config);
+            new TypeError("targetService.validateConfig should fail");
+        }
+        catch (error) {
+            expect(error.message).toBe("targetConfig.organizationId must be provided");
+        }
+
+        try {
+            config.organizationId = false;
+            targetService.validateConfig(config);
+            new TypeError("targetService.validateConfig should fail");
+        }
+        catch (error) {
+            expect(error.message).toBe("targetConfig.organizationId must be provided");
+        }
+    });
+
+    it("fails if targetConfig.organizationId is not a string", () => {
+        const targetService = getTargetService(targetConfig);
+        const config = { client: "client" };
+
+        try {
+            config.organizationId = 8;
+            targetService.validateConfig(config);
+            new TypeError("targetService.validateConfig should fail");
+        }
+        catch (error) {
+            expect(error.message).toBe("targetConfig.organizationId must be a string");
+        }
+
+        try {
+            config.organizationId = true;
+            targetService.validateConfig(config);
+            new TypeError("targetService.validateConfig should fail");
+        }
+        catch (error) {
+            expect(error.message).toBe("targetConfig.organizationId must be a string");
+        }
+
+        try {
+            config.organizationId = {};
+            targetService.validateConfig(config);
+            new TypeError("targetService.validateConfig should fail");
+        }
+        catch (error) {
+            expect(error.message).toBe("targetConfig.organizationId must be a string");
+        }
+    });
+
+    it("fails if targetConfig.propertyToken is falsy", () => {
+        const targetService = getTargetService(targetConfig);
+        const config = {
+            client: "client",
+            organizationId: "organizationId"
+        }
+
+        try {
+            targetService.validateConfig(config);
+            new TypeError("targetService.validateConfig should fail");
+        }
+        catch (error) {
+            expect(error.message).toBe("targetConfig.propertyToken must be provided");
+        }
+
+        try {
+            config.propertyToken = undefined;
+            targetService.validateConfig(config);
+            new TypeError("targetService.validateConfig should fail");
+        }
+        catch (error) {
+            expect(error.message).toBe("targetConfig.propertyToken must be provided");
+        }
+
+        try {
+            config.propertyToken = null;
+            targetService.validateConfig(config);
+            new TypeError("targetService.validateConfig should fail");
+        }
+        catch (error) {
+            expect(error.message).toBe("targetConfig.propertyToken must be provided");
+        }
+
+        try {
+            config.propertyToken = false;
+            targetService.validateConfig(config);
+            new TypeError("targetService.validateConfig should fail");
+        }
+        catch (error) {
+            expect(error.message).toBe("targetConfig.propertyToken must be provided");
+        }
+    });
+
+    it("fails if targetConfig.propertyToken is not a string", () => {
+        const targetService = getTargetService(targetConfig);
+        const config = {
+            client: "client",
+            organizationId: "organizationId"
+        }
+
+        try {
+            config.propertyToken = 8;
+            targetService.validateConfig(config);
+            new TypeError("targetService.validateConfig should fail");
+        }
+        catch (error) {
+            expect(error.message).toBe("targetConfig.propertyToken must be a string");
+        }
+
+        try {
+            config.propertyToken = true;
+            targetService.validateConfig(config);
+            new TypeError("targetService.validateConfig should fail");
+        }
+        catch (error) {
+            expect(error.message).toBe("targetConfig.propertyToken must be a string");
+        }
+
+        try {
+            config.propertyToken = {};
+            targetService.validateConfig(config);
+            new TypeError("targetService.validateConfig should fail");
+        }
+        catch (error) {
+            expect(error.message).toBe("targetConfig.propertyToken must be a string");
+        }
     });
 });
-
-describe("TargetService.getActivities", () => {});
-
-describe("TargetService.sendNotification", () => {});
-
-describe("TargetService.targetServiceConstructor", () => {});
-
-describe("TargetService.getActivitiesRoute", () => {});
-
-describe("TargetService.sendNotificationRoute", () => {});
-
-describe("TargetService.getActivitiesMiddleware", () => {});
-
-describe("TargetService.sendNotificationMiddleware", () => {});
